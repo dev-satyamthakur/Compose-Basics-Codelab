639548c9bb239d83b918995d074d9695a6b68d05
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-05 21:48:03 +0530

    Using modifiers to resize composables

--- app/src/main/java/com/satyamthakur/learning/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/composebasicscodelab/MainActivity.kt
@@ -3,6 +3,8 @@ package com.satyamthakur.learning.composebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,6 +13,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.satyamthakur.learning.composebasicscodelab.ui.theme.ComposeBasicsCodelabTheme
@@ -27,28 +30,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String) {
     Surface(
-        color = MaterialTheme.colorScheme.primary
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(16.dp)
-        )
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
+            Text(
+                text = "Hello,"
+            )
+            Text(
+                text = "$name!"
+            )
+        }
+
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun GreetingPreview() {
     ComposeBasicsCodelabTheme {
