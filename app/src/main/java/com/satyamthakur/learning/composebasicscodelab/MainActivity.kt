fb88095cd9f527703e291a44fc6a90520d0bb90f
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-06 16:16:18 +0530

    Using state to manipulate the UI

--- app/src/main/java/com/satyamthakur/learning/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/composebasicscodelab/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.satyamthakur.learning.composebasicscodelab
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,28 +38,48 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyApp(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = listOf("World", "Compose") /*"Aman", "Satyam",
+        "Jyotirmoy", "Snigdha", "Raksha", "Vedant", "Nikhil", "Ashwin", "Suraj")*/
 ) {
+//    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+//        items(names.size) {
+//            Greeting(names.get(it))
+//        }
+//    }
+
     Column(modifier = modifier.padding(vertical = 4.dp)) {
         for (name in names) {
-            Greeting(name = name)
+            Greeting(name)
         }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp)) {
-            Text(
-                text = "Hello,"
-            )
-            Text(
-                text = "$name!"
-            )
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(
+                    text = "Hello,"
+                )
+                Text(
+                    text = "$name!"
+                )
+            }
+            ElevatedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(if (expanded.value) "Show less" else "Show More")
+                Log.d("MYAPP", "Sample Log")
+            }
         }
 
     }
