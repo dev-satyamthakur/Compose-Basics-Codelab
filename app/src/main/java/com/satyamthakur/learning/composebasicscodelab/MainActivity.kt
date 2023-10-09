46b8603df70f5cacb296764bed455f9d096cf92a
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-09 21:08:39 +0530

    Creating a performant using LazyColumn

--- app/src/main/java/com/satyamthakur/learning/composebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/satyamthakur/learning/composebasicscodelab/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
@@ -23,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.lazy.items
 import com.satyamthakur.learning.composebasicscodelab.ui.theme.ComposeBasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -83,10 +85,12 @@ fun OnboardingScreen(
 @Composable
 private fun Greetings(
     modifier: Modifier = Modifier,
-    names: List<String> = listOf("World", "Compose")
+    names: List<String> = List(1000) { "$it" }
 ) {
-    Column(modifier = modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
+    LazyColumn(
+        modifier = modifier.padding(vertical = 4.dp)
+    ) {
+        items(items = names) { name->
             Greeting(name = name)
         }
     }
